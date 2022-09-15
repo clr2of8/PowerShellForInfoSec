16b0c86a2d4f14710ab2ab2cd974366a4286ea33
Carrie Roberts <clr2of8@gmail.com>
2022-09-14 18:23:06 -0600

    Create PSwoPS.cs

---
+++ Tools/PSwoPS.cs
@@ -0,0 +1,59 @@
+using System;
+
+ 
+ //Add For PowerShell Invocation
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+
+/*
+Simplified version of code originally authored by: Casey Smith, Twitter: @subTee
+
+License: BSD 3-Clause
+
+Compile with:
+C:\Windows\Microsoft.NET\Framework64\v2.0.50727\csc.exe /r:C:\Windows\assembly\GAC_MSIL\System.Management.Automation\1.0.0.0__31bf3856ad364e35\System.Management.Automation.dll /unsafe /platform:anycpu /out:PSwoPS.exe C:\Users\IEUser\PowerShellForInfoSec\Tools\PSwoPS.cs
+*/
+
+ public class Program
+ {
+ public static void Main()
+ {
+		while(true)
+		{
+			
+			Console.Write("PS without PS >");
+			string x = Console.ReadLine();
+			Console.WriteLine(RunPSCommand(x));
+			
+		}
+ }
+ 
+ 	//Based on Jared Atkinson's And Justin Warner's Work
+	public static string RunPSCommand(string cmd)
+	{
+		//Init stuff
+		Runspace runspace = RunspaceFactory.CreateRunspace();
+		runspace.Open();
+		RunspaceInvoke scriptInvoker = new RunspaceInvoke(runspace);
+		Pipeline pipeline = runspace.CreatePipeline();
+
+		//Add commands
+		pipeline.Commands.AddScript(cmd);
+
+		//Prep PS for string output and invoke
+		pipeline.Commands.Add("Out-String");
+		Collection<PSObject> results = pipeline.Invoke();
+		runspace.Close();
+
+		//Convert records to strings
+		StringBuilder stringBuilder = new StringBuilder();
+		foreach (PSObject obj in results)
+		{
+			stringBuilder.Append(obj);
+		}
+		return stringBuilder.ToString().Trim();
+	 }
+	 
+ }
