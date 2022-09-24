4128e394330946edabec62f2acb168f3d6a56dac
Carrie Roberts <clr2of8@gmail.com>
2022-09-24 15:00:05 -0600

    Update vm-setup.ps1

--- Tools/vm-setup.ps1
+++ Tools/vm-setup.ps1
@@ -103,7 +103,7 @@ if ($VMtype -eq "2") {
     $computerName = "PS4I-REMOTE"
     New-Item -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies -Name System -Force | Out-Null
     Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name Wallpaper -Value "C:\Windows\Web\Wallpaper\Theme1\img4.jpg" | Out-Null
-    Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name WallpaperStyle -Value 4 | Out-Null
+    Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name WallpaperStyle -Value "4" | Out-Null
     Add-TestUsers
 }
 if (
@@ -111,7 +111,7 @@ if (
     $computerName = "PS4I-REMOTE-2"
     New-Item -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies -Name System -Force | Out-Null
     Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name Wallpaper -Value "C:\Windows\Web\Wallpaper\Theme1\img2.jpg" | Out-Null
-    Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name WallpaperStyle -Value 4 | Out-Null
+    Set-ItemProperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name WallpaperStyle -Value "4" | Out-Null
     Add-TestUsers
 }
 if ($env:COMPUTERNAME -ne $computerName) {
