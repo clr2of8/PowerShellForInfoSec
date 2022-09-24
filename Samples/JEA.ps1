65a2cc6d892df670962770f3f26b7043fa20ee10
Carrie Roberts <clr2of8@gmail.com>
2022-09-23 21:20:21 -0600

    Update JEA.ps1

--- Samples/JEA.ps1
+++ Samples/JEA.ps1
@@ -1,25 +1,31 @@
-$capabilityName = 'SpoolerRestart'
-
 ############################################################################################
 ### Create the RoleCapabilities file (psrc) inside of a <module>\RoleCapabilities folder ###
 ############################################################################################
 
-$roleCapabilitiesPath = "C:\Program Files\WindowsPowerShell\Modules\$capabilityName\RoleCapabilities"
+$roleCapabilitiesPath = "C:\Program Files\WindowsPowerShell\Modules\SpoolerRestart\RoleCapabilities"
 New-Item -ItemType Directory $roleCapabilitiesPath -ErrorAction Ignore
 
 $VisibleCmdlets = 'Get-Service',
                   @{ Name = 'Restart-Service'; Parameters = @{ Name = 'Name'; ValidateSet = 'Spooler' }},
                   @{ Name = 'Stop-Service'; Parameters = @{ Name = 'Name'; ValidateSet = 'Spooler' }}
 
-New-PSRoleCapabilityFile -Path "$roleCapabilitiesPath\$capabilityName.psrc" -VisibleCmdlets $visibleCmdlets
+
+New-PSRoleCapabilityFile -Path "$roleCapabilitiesPath\SpoolerRestart.psrc" -VisibleCmdlets $visibleCmdlets
+
+#############################################################################
+### Create the Session Configuration file (pssc) in a temporary directory ###
+#############################################################################
+
+$args = @{
+ Path = "$env:Temp\SpoolerRestart.pssc"
+ RoleDefinitions = @{ 'PrinterAdmins' = @{RoleCapabilities = "SpooolerRestart" }}
+ SessionType = "RestrictedRemoteServer"
+ RunAsVirtualAccount = $true
+}
+New-PSSessionConfigurationFile @args
 
 ###############################################################
 #### Register the endpoint that the session will connect to ###
 ###############################################################
-
-$sessionEndPointName = "$capabilityName`Endpoint"
-
-$roleDefinitions = @{ 'PrinterAdmins' = @{RoleCapabilities = $capabilityName }}
-
-New-PSSessionConfigurationFile -Path $env:Temp\$capabilityName.pssc -RoleDefinitions $roleDefinitions -SessionType RestrictedRemoteServer -RunAsVirtualAccount 
-Register-PSSessionConfiguration -Name $sessionEndPointName -Path $env:Temp\$capabilityName.pssc -Force
+ 
+Register-PSSessionConfiguration -Name "SpoolerRestartEndpoint" -Path $env:Temp\SpoolerRestart.pssc -Force
