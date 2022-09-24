647b9793b259fad0d2f8904e1964505134102457
Carrie Roberts <clr2of8@gmail.com>
2022-09-24 16:33:37 -0600

    Update Set-PSLogging.ps1

--- Tools/Set-PSLogging.ps1
+++ Tools/Set-PSLogging.ps1
@@ -215,31 +215,3 @@ function Show-AllLogging {
     Set-PSModuleLogging -show
     Set-PSTranscriptionLogging -show
 } 
-
-$options = New-Object System.Collections.Generic.Dictionary"[Int,String]"
-$options.add(1, "Enable-AllREasonableLogging")
-$options.add(2, "Disable-AllLogging")
-$options.add(3, "Show-AllLogging")
-$options.add(4, "Enable-AllLogging")
-
-
-while ($true) {
-    Clear-Host
-    Write-Host -ForegroundColor Yellow "**********************************************"
-    Write-Host -ForegroundColor Yellow "Select your option:`n"
-
-    foreach ($key in $options.keys) {
-        Write-Host -ForegroundColor Yellow "$key) $($options[$key])"
-    }
-
-    Write-Host -ForegroundColor Yellow "`n*************************************************"
-    Write-Host
-
-    $optionSelected = Read-Host "Select your option (or q to quit) "
-    if ($optionSelected -eq "q") { break }
-    if($options.keys -contains $optionSelected){
-        & $options[$optionSelected]
-        Write-Host -NoNewLine "`nPress any key to continue..."
-        $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
-    }
-}
