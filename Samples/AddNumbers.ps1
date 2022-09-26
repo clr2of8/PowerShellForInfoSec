510d305d694d652d9d38c5b2b3239dcb49e039f6
Carrie Roberts <clr2of8@gmail.com>
2022-09-26 16:05:51 -0600

    Create AddNumbers.ps1

---
+++ Samples/AddNumbers.ps1
@@ -0,0 +1,11 @@
+function add($a,$b){ 
+    return $a + $b
+}
+
+Write-Host -ForegroundColor Cyan 1
+$x = 0
+$x = add 1 1
+$x = add 2 2
+Write-Host -ForegroundColor Cyan 2
+Write-Host -ForegroundColor Cyan 3
+$x
