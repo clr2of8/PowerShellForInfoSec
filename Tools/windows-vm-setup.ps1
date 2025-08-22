df2239f3904c70b42a9d89e64abf5f7f74e7fc35
Carrie Roberts <clr2of8@gmail.com>
2025-08-22 16:46:08 -0600

    Update windows-vm-setup.ps1

--- Tools/windows-vm-setup.ps1
+++ Tools/windows-vm-setup.ps1
@@ -71,7 +71,7 @@ Write-Host "Writing class files to $env:USERPROFILE\PowerShellForInfoSec" -Foreg
 Get-ClassFiles
 # compile log watcher tool and put on the desktop
 Stop-Process -Name TailPSopLog -ErrorAction Ignore
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:C:\Users\art\Desktop\TailPSopLog.exe C:\Users\art\PowerShellForInfoSec\Tools\TailPSopLog.cs
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:C:\Users\art\Desktop\TailPSopLog.exe C:\Users\art\PowerShellForInfoSec\Tools\TailPSopLog.cs | Out-Null
 
 # add Desktop shortcuts
 Write-Host "Creating Desktop Shortcuts" -ForegroundColor Cyan
