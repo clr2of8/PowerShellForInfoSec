0d8475a09a9e24d8b2ef97c45b2fba7adae91dcf
Carrie Roberts <clr2of8@gmail.com>
2025-08-22 18:59:50 -0600

    Update DownloadAndImport-Obfuscation.ps1

--- Tools/DownloadAndImport-Obfuscation.ps1
+++ Tools/DownloadAndImport-Obfuscation.ps1
@@ -4,10 +4,10 @@ Write-Host -ForegroundColor Yellow "Disabling AV"
 Set-MpPreference -DisableRealtimeMonitoring $true
 
 Write-Host -ForegroundColor Yellow "Downloading and Extracting Invoke-Obfuscation"
-Invoke-WebRequest 'https://github.com/danielbohannon/Invoke-Obfuscation/archive/refs/heads/master.zip' -OutFile 'C:\Users\IEUser\PowerShellForInfoSec\Tools\Invoke-Obfuscation.zip'
-Expand-Archive  'C:\Users\IEUser\PowerShellForInfoSec\Tools\Invoke-Obfuscation.zip' -Force -DestinationPath 'C:\Users\IEUser\PowerShellForInfoSec\Tools\'
+Invoke-WebRequest 'https://github.com/danielbohannon/Invoke-Obfuscation/archive/refs/heads/master.zip' -OutFile "$env:USERPROFILE\PowerShellForInfoSec\Tools\Invoke-Obfuscation.zip"
+Expand-Archive  "$env:USERPROFILE\PowerShellForInfoSec\Tools\Invoke-Obfuscation.zip" -Force -DestinationPath "$env:USERPROFILE\PowerShellForInfoSec\Tools\"
 
 Write-Host -ForegroundColor Yellow "Importing Invoke-Obfuscation"
-Import-Module 'C:\Users\IEUser\PowerShellForInfoSec\Tools\Invoke-Obfuscation-master\Invoke-Obfuscation.psd1' -Force
+Import-Module "$env:USERPROFILE\PowerShellForInfoSec\Tools\Invoke-Obfuscation-master\Invoke-Obfuscation.psd1" -Force
 
 Write-Host -ForegroundColor Green "Done! Use 'Invoke-Obfuscation' from this session to run the tool"
