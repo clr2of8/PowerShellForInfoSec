a781b22b4aefed00d35096521a65c8ddf7dfeef0
Carrie Roberts <clr2of8@gmail.com>
2025-10-15 20:01:24 -0400

    Update windows-vm-setup.ps1

--- Tools/windows-vm-setup.ps1
+++ Tools/windows-vm-setup.ps1
@@ -7,13 +7,7 @@ iex (iwr https://raw.githubusercontent.com/clr2of8/PowerShellForInfoSec/refs/hea
 $uuid = (Get-WmiObject -Query "select uuid from Win32_ComputerSystemProduct").UUID
 if ($uuid -like "EC2*") { } else {
     # local VM
-    # add local domain to hosts file for remote.cloudlab.lan
-    $hostsPath = "$env:SystemRoot\System32\drivers\etc\hosts"
-    $entry = "192.168.195.136`tremote.cloudlab.lan"
-    $hostsContent = Get-Content $hostsPath -ErrorAction SilentlyContinue
-    if ($hostsContent -notmatch "remote\.cloudlab\.lan") {
-        Add-Content -Path $hostsPath -Value $entry
-        Write-Host "Added remote.cloudlab.lan to hosts file." -ForegroundColor Cyan
+    Purple-AddRemoteVMHostsEntry
     }
     else {
         Write-Host "Entry for remote.cloudlab.lan already exists in hosts file." -ForegroundColor Yellow
